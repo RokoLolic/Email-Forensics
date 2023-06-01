2b32e30db8236cf95cf6b823527b6d8b62cb682e
RokoLolic <rlolic00@fesb.hr>
2023-06-01 22:43:11 +0200

    writes out SCL of email message

--- readheader.py
+++ readheader.py
@@ -13,7 +13,7 @@ def extractIPs(fileContent):
     ipv6s = re.findall(ipv6_extract_pattern, fileContent)
     return ipv4s, ipv6s
 
-filename = "va-je-google-raun-uspjeno-oporavljen.eml"
+filename = "hello from outlook.eml"
 
 # If the e-mail headers are in a file, uncomment these two lines:
 with open(filename, 'rb') as fp:
@@ -25,55 +25,69 @@ print('From: {}'.format(headers['from']))
 print('Subject: {}'.format(headers['subject']))
 
 # You can also access the parts of the addresses:
-print('Recipient username: {}'.format(headers['to'].addresses[0].username))
-print('Sender name: {}'.format(headers['from'].addresses[0].display_name))
+#print('Recipient username: {}'.format(headers['to'].addresses[0].username))
+#print('Sender name: {}'.format(headers['from'].addresses[0].display_name))
 #print(headers)
 headers_string = str(headers)
 #print(headers_string)
 IPV4s, IPV6s= extractIPs(headers_string) # extract IPv4 and IPv6 adresses
-
+#print(headers_string)
 
 foundadresses = []
-for i in range(len(IPV6s)): 
-    ip_address = IPV6s[i][0]
-    print(ip_address)
-    request_url = 'https://geolocation-db.com/jsonp/' + ip_address
-    response = requests.get(request_url)
-    result = response.content.decode()
-    result = result.split("(")[1].strip(")")
-    result  = json.loads(result)
-    result_latitude = result['latitude']
-    if result_latitude == "Not found":
-        continue
-    result_longitude = result['longitude']
+# for i in range(len(IPV6s)): 
+#     ip_address = IPV6s[i][0]
+#     print(ip_address)
+#     request_url = 'https://geolocation-db.com/jsonp/' + ip_address
+#     response = requests.get(request_url)
+#     result = response.content.decode()
+#     result = result.split("(")[1].strip(")")
+#     result  = json.loads(result)
+#     result_latitude = result['latitude']
+#     if result_latitude == "Not found":
+#         continue
+#     result_longitude = result['longitude']
 
-    if [result_latitude, result_longitude] in foundadresses:
-        continue
-    foundadresses.append([result_latitude, result_longitude])
+#     if [result_latitude, result_longitude] in foundadresses:
+#         continue
+#     foundadresses.append([result_latitude, result_longitude])
     
-    url="http://www.google.com/maps/place/" + str(result_latitude) + "," + str(result_longitude)
+#     url="http://www.google.com/maps/place/" + str(result_latitude) + "," + str(result_longitude)
 
-    webbrowser.open_new_tab(url)
+#     webbrowser.open_new_tab(url)
 
 
-for i in range(len(IPV4s)):
-    ip_address = IPV4s[i]
-    print(ip_address)
-    request_url = 'https://geolocation-db.com/jsonp/' + ip_address
-    response = requests.get(request_url)
-    result = response.content.decode()
-    result = result.split("(")[1].strip(")")
-    result  = json.loads(result)
-    result_latitude = result['latitude']
-    if result_latitude == "Not found":
-        continue
-    result_longitude = result['longitude']
+# for i in range(len(IPV4s)):
+#     ip_address = IPV4s[i]
+#     print(ip_address)
+#     request_url = 'https://geolocation-db.com/jsonp/' + ip_address
+#     response = requests.get(request_url)
+#     result = response.content.decode()
+#     result = result.split("(")[1].strip(")")
+#     result  = json.loads(result)
+#     result_latitude = result['latitude']
+#     if result_latitude == "Not found":
+#         continue
+#     result_longitude = result['longitude']
    
-    if [result_latitude, result_longitude] in foundadresses:
-        continue
-    foundadresses.append([result_latitude, result_longitude])
+#     if [result_latitude, result_longitude] in foundadresses:
+#         continue
+#     foundadresses.append([result_latitude, result_longitude])
     
-    url="http://www.google.com/maps/place/" + str(result_latitude) + "," + str(result_longitude)
-    webbrowser.open_new_tab(url)
+#     url="http://www.google.com/maps/place/" + str(result_latitude) + "," + str(result_longitude)
+#     webbrowser.open_new_tab(url)
 
-print(foundadresses)
+#print(foundadresses)
+
+if "SCL:" in headers_string:
+    location = headers_string.index("SCL:")
+    #print(headers_string[location])
+    if headers_string[location+4]==" " :
+        if(headers_string[location+5]=="-" ):
+            print(f"Nije spam, SCL: -{headers_string[location+6]}")
+        else:
+            print(f"SCL: {headers_string[location+5]}")
+    else:
+        if(headers_string[location+4]=="-" ):
+            print(f"Nije spam, SCL: {headers_string[location+5]}")
+        else:
+            print(f"SCL: {headers_string[location+4]}")
